8c95db11f7834d17548fcd9ef6c7f6803b44f324
athulanil17 <118271019+athulanil17@users.noreply.github.com>
2023-01-23 11:54:04 +0530

    Update Readlinebyline.py

--- Readlinebyline.py
+++ Readlinebyline.py
@@ -1,10 +1,5 @@
-
-
 f= open("text.txt")
-    
 content_list = f.readlines()
-
-print(content_list)
-
 content_list = [x.strip() for x in content_list]
 print(content_list)
+print(type(content_list))
